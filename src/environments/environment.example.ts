6d324be589139be1ff3554480919d659b86f2acf
kennethdossou-sys <kennethdossou300@gmail.com>
2026-05-10 12:42:24 +0000

    Sécurisation des clées Firebase - environment.ts ignoré

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production:false,
+    firebase:{
+        apiKey: "VOTRE_API_KEY",
+        authDomain:"VOTRE_AUTH_DOMAIN",
+        databaseURL:"VOTRE_DATABASE_URL",
+        projectID: "VOTRE_PROJECT_ID",
+        appID:"VOTRE_APP_ID"
+    }
+};
